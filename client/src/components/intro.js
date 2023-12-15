e892eafc330be83f27bc12afaad25108a6ddaaad
tyler2089 <tyguy208@gmail.com>
2023-12-15 09:27:04 -0800

    fixed video bug

--- client/src/components/intro.js
+++ client/src/components/intro.js
@@ -19,10 +19,20 @@ function Intro() {
   }, [intro]);
   return (
     <div className="intro-container" ref={container}>
-      <video autoPlay="autoplay" muted className="intro" ref={intro}>
+      <video
+        autoPlay="autoplay"
+        muted
+        className="intro"
+        ref={intro}
+        playsinline="playsinline"
+      >
         <source
           src={require("../static/catherineintrotest.mp4")}
           type="video/mp4"
+        ></source>
+        <source
+          src={require("../static/catherineintrotest.webm")}
+          type="video/webm"
         ></source>
       </video>
       <div className="not-revealed" ref={arrow}>
